a97a6a65d9cc9dda6c7a3b4f85d0a787bb81edf2
h.sekiya <sky.happymail23@gmail.com>
2024-11-13 17:15:15 +0900

    add:developとfeature/202411/sekiyaのブランチを追加

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemsController;
 
 Route::get('/', [ItemsController::class, 'index']);
+Route::get('/items', [ItemsController::class, 'index']);
